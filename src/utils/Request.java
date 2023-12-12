6343314307f55f62bb3f8ecb3d0ed9eb216e6031
QuanyshK <zoomer.qurbany@gmail.com>
2023-12-12 22:49:08 +0600

    employee, teacher, dean

--- src/utils/Request.java
+++ src/utils/Request.java
@@ -4,23 +4,23 @@ import enums.*;
 import wsp.*;
 
 public class Request {
-	
-	private Emergency emergency;
-	private String message;
-	private Dean to;
-	
-	
-	public Request(Emergency emergency, String message, Dean to) {
-		this.emergency = emergency;
-		this.message = message;
-		this.to = to;
-	}
+
+    private Emergency emergency;
+    private String message;
+    private Dean to;
 
 
-	@Override
-	public String toString() {
-		return "Request [emergency=" + emergency + ", message=" + message + ", to=" + to + "]";
-	}
-	
-	
+    public Request(Emergency emergency, String message, Dean to) {
+        this.emergency = emergency;
+        this.message = message;
+        this.to = to;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Request [emergency=" + emergency + ", message=" + message + ", to=" + to + "]";
+    }
+
+
 }
