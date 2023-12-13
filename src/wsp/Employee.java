9ed05ef159646d36546c481b84b377a5006df647
magzhanmnazhatdin <79352934+magzhanmnazhatdin@users.noreply.github.com>
2023-12-13 14:01:04 +0600

    Create Employee.java

---
+++ src/wsp/Employee.java
@@ -0,0 +1,42 @@
+package wsp;
+
+import enums.Emergency;
+import utils.Order;
+import utils.Request;
+
+public class Employee extends User {
+	
+	public Employee() {
+		
+	}
+
+	public Employee(String username, String password, String firstName, String lastName) {
+		super(username, password, firstName, lastName);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void sendMessage(String message, User user) {
+		
+	}
+	
+	public void sendRequest(Request req) {
+		
+	}
+	
+	public void sendOrder(Order order) {
+		
+	}
+	
+	@Override
+	public void viewNews() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void viewMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
