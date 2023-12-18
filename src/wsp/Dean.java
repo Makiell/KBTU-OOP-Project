a6694769188204b931e8da7acd371ff31249f428
nooloudy <n_orynbassar@kbtu.kz>
2023-12-18 22:46:38 +0600

    update Dean

--- src/wsp/Dean.java
+++ src/wsp/Dean.java
@@ -1,102 +1,83 @@
 package wsp;
 
 import java.io.Serializable;
-import enums.*;
-
-import java.util.Vector;
-
-import database.Database;
-
-import java.util.Scanner;
 import enums.Faculty;
-import enums.TeacherTitle;
-import utils.*;
-import utils.Course;
-import utils.Lesson;
-	
-
+import java.util.Vector;
+import utils.Request;
+import utils.StaticMethods;
 
 public class Dean extends Teacher implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	Vector <Request> requests = new Vector<>();
-    public Dean(String username, String password, String firstName, String lastName,
-                Faculty faculty) {
+    private static final long serialVersionUID = 1L;
+    private Vector<Request> requests = new Vector<>();
+
+    public Dean(String username, String password, String firstName, String lastName, Faculty faculty) {
         super(username, password, firstName, lastName, null, faculty);
-        // TODO Auto-generated constructor stub
     }
-    public Vector<Request> getRequests(){
+
+    public Vector<Request> getRequests() {
         return requests;
     }
 
-    public void setRequests(Vector <Request> requests) {
+    public void setRequests(Vector<Request> requests) {
         this.requests = requests;
     }
 
     public void signRequests() {
-    	while(true) {
-	    	if(requests.isEmpty()) {
-	    		System.out.println("No requests");
-	    		break;
-	    	}
-	    	
-	    	System.out.println("----Emergency Requests----");
-	    	for (int i = 0; i<requests.size(); i++) {
-	    		System.out.println((i + 1) + ". " + requests.get(i));
-	    	}
-	    	
-	    	System.out.println("Enter the request number to sign (or 0 to exit):");
-	    	int choice = StaticMethods.validate(requests.size());
-	    	
-	    	if (choice == 0) {
-	    		break;
-	    	}
-	
-	    	int index = choice - 1;
-	    	
-	    	if (index >= 0 && index < requests.size()) {
-	    		Request selectedRequest = requests.get(index);
-	    		System.out.println("Dean signed request: " + selectedRequest);
-	    		
-	    		requests.remove(selectedRequest);
-	    		
-	    		System.out.println("The request was successfully signed and deleted.");
-	    	}
-	    	else {
-	    		System.out.println("Invalid request number.");
-	    	}
-    	}
+        while (true) {
+            if (requests.isEmpty()) {
+                System.out.println("No requests");
+                break;
+            }
+
+            System.out.println("----Emergency Requests----");
+            for (int i = 0; i < requests.size(); i++) {
+                System.out.println((i + 1) + ". " + requests.get(i));
+            }
+
+            System.out.println("Enter the request number to sign (or 0 to exit):");
+            int choice = StaticMethods.validate(requests.size() + 1);
+
+            if (choice == 0) {
+                break;
+            }
+
+            int index = choice - 1;
+
+            Request selectedRequest = requests.get(index);
+            System.out.println("Dean signed request: " + selectedRequest);
+
+            requests.remove(selectedRequest);
+
+            System.out.println("The request was successfully signed and deleted.");
+        }
     }
-    
+
     public void viewMenu() {
-    	String[] options = new String[]{
-                "Sign Requests", "Exit"
-        };
-    	
-    	while (true) {
-    		System.out.println();
-    		System.out.println("----Dean Menu----");
-    		for (int i = 0; i<options.length; i++) {
-    			System.out.println((i + 1) + ". " + options[i]);
-    		}
-    		
-    		int choice = StaticMethods.validate(options.length);
-    		if(choice == 1) {
-    			signRequests();
-    		}
+        String[] options = new String[]{"Sign Requests", "Exit"};
 
-    		else if (choice == 2) {
-    			break;
-    		}
-    	
-    	}
+        while (true) {
+            System.out.println();
+            System.out.println("----Dean Menu----");
+            for (int i = 0; i < options.length; i++) {
+                System.out.println((i + 1) + ". " + options[i]);
+            }
+
+            int choice = StaticMethods.validate(options.length);
+            if (choice == 1) {
+                signRequests();
+            } else if (choice == 2) {
+                break;
+            }
+        }
     }
 
-    public void addRequest(Request request){
+    public void addRequest(Request request) {
         requests.add(request);
     }
-    
+
+    @Override
     public String toString() {
-    	return "Dean " + super.toString();
+        return "Dean " + super.toString();
     }
 }
