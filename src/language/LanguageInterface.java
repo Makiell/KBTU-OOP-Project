2c8a6ec14faeec2a78a4ea8b573a3e2ba9e90f8f
nooloudy <n_orynbassar@kbtu.kz>
2023-12-25 19:56:01 +0600

    interfaceLanguage

---
+++ src/language/LanguageInterface.java
@@ -0,0 +1,37 @@
+package language;
+
+
+public interface LanguageInterface {
+
+    String[] studentMenu();
+
+    String[] studentResearcherMenu();
+    
+    String[] teacherMenu();
+    
+    String[] teacherResearcherMenu();
+    
+    String[] adminMenu();
+    
+    String[] managerMenu();
+    
+    String[] managerResearcherMenu();
+    
+    String[] deanMenu();
+    
+    String[] deanResearcherMenu();
+    
+    String[] techSupportSpecialistMenu();
+    
+    String[] techSupportSpecialistResearcherMenu();
+    
+    String[] graduateStudentMenu();
+    
+    String[] employeeMenu();
+    
+    String[] employeeResearcherMenu();
+    
+    String[] researcherMenu();
+    
+}
+
